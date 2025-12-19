7222c8012d4f7dbde871b0d91e5f9999634ad39e
abraham <abrahamkifle1212@gmail.com>
2025-12-19 22:06:05 +0300

    finish hero component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import Navbar from './components/navbar'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 import './index.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
