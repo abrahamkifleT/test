8e8594d45f135e1b1768e6c79b1b9d552b350623
abraham <abrahamkifle1212@gmail.com>
2025-12-19 17:00:55 +0300

    adding navbar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/navbar'
 import './index.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <div>
-
+      <Navbar />
     </div>
   )
 }
