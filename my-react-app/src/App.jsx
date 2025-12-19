385f8ad744bc4367d90582fa8f7d287d2864dad3
abraham <abrahamkifle1212@gmail.com>
2025-12-19 23:35:49 +0300

    adding analytics

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Analytics from './components/Analytics'
 import './index.css'
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   )
 }
