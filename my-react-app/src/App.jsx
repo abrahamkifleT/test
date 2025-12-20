5618609b5ab122ae9ae64384e5e5c8bb87d1de1e
abraham <abrahamkifle1212@gmail.com>
2025-12-20 09:22:20 +0300

    feat: Add and integrate a new Newsletter component into the application.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Analytics from './components/Analytics'
 import './index.css'
+import Newsletter from './components/Newsletter'
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   )
 }
